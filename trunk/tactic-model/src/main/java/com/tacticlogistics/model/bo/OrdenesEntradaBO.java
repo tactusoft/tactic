540c0050084dc35628fa81b94110f02e97bf4856
carlossarmientor <carlossarmientor@0972d321-b093-c4ff-da88-44c8b6e2f0b3>
2015-08-24 11:24:37 +0000

    Inicial Import

---
+++ trunk/tactic-model/src/main/java/com/tacticlogistics/model/bo/OrdenesEntradaBO.java
@@ -0,0 +1,23 @@
+package com.tacticlogistics.model.bo;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tacticlogistics.model.dao.CustomHibernateDao;
+import com.tacticlogistics.model.entities.Pais;
+
+@Service
+public class OrdenesEntradaBO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Autowired
+	private CustomHibernateDao dao;
+
+	public List<Pais> getListPaises() {
+		return dao.find("from Pais");
+	}
+
+}
