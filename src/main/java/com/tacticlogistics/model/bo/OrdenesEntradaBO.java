1099c2b17943e60ec7dde8633ed103b487650e3c
csarmiento <tactusoft@hotmail.com>
2015-09-28 16:45:52 -0500

    Modelo de Datos

--- src/main/java/com/tacticlogistics/model/bo/OrdenesEntradaBO.java
+++ src/main/java/com/tacticlogistics/model/bo/OrdenesEntradaBO.java
@@ -1,14 +1,11 @@
 package com.tacticlogistics.model.bo;
 
 import java.io.Serializable;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tacticlogistics.model.dao.CustomHibernateDao;
-import com.tacticlogistics.model.entities.Ciudad;
-import com.tacticlogistics.model.entities.Pais;
 
 @Service
 public class OrdenesEntradaBO implements Serializable {
@@ -17,12 +14,12 @@ public class OrdenesEntradaBO implements Serializable {
 	@Autowired
 	private CustomHibernateDao dao;
 
-	public List<Pais> getListPaises() {
+	/*public List<Pais> getListPaises() {
 		return dao.find("from Pais");
 	}
 
 	public List<Ciudad> getListCiudades(Integer idPais) {
 		return dao.find("from Ciudad o where o.pais.id = " + idPais);
-	}
+	}*/
 
 }
